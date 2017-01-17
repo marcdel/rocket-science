5d7547b4fc6c5cffe34a503221f09fba4b109403
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-01-16 21:52:11 -0800

    Add converter module.

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,9 +1,29 @@
+defmodule Converter do
+  def to_nearest_tenth(val) do
+    Float.ceil val, 1
+  end
+
+  def to_km(velocity) do
+    velocity / 1000
+  end
+
+  def to_light_seconds({:miles, miles}) do
+    (miles * 5.36819e-6) |> round_down
+  end
+
+  def to_light_seconds({:meters, meters}) do
+    (meters * 3.335638620368e-9) |> round_down
+  end
+
+  def round_down(val) when is_float(val), do: trunc(val)
+end
+
 defmodule Physics.Rocketry do
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
-      |> convert_to_km
-      |> rounded_to_nearest_tenth
+      |> Converter.to_km
+      |> Converter.to_nearest_tenth
   end
 
   defp rounded_to_nearest_tenth(val) do
