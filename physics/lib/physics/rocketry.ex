a45773c961fb0cab46a3fc46c34f44a30fd09133
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-01-18 08:31:03 -0800

    Refactor converter.

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -7,15 +7,35 @@ defmodule Converter do
     velocity / 1000
   end
 
-  def to_light_seconds({:miles, miles}) do
-    (miles * 5.36819e-6) |> round_down
+  def to_light_seconds({:miles, miles}, precision: precision) do
+    (miles * 5.36819e-6) |> round_to
   end
 
-  def to_light_seconds({:meters, meters}) do
-    (meters * 3.335638620368e-9) |> round_down
+  def to_light_seconds({:meters, meters}, precision: precision) do
+    (meters * 3.335638620368e-9) |> round_to
   end
 
-  def round_down(val) when is_float(val), do: trunc(val)
+  def round_to(val, precision \\ 5) when is_float(val) do
+    Float.round(val, precision)
+  end
+end
+
+defmodule ConverterTwo do
+  def to_light_seconds({unit, val}, precision: precision) do 
+    case unit do 
+      :miles -> from_miles(val)
+      :meters -> from_meters(val)
+      :feet -> from_feet(val)
+      :inches -> from_inches(val)
+    end |> round_to(precision)
+  end
+
+  defp from_miles(val), do: val * 5.36819e-6
+  defp from_meters(val), do: val * 3.335638620368e-9
+  defp from_feet(val), do: val * 1.016702651488166404e-9
+  defp from_inches(val), do: val * 8.472522095734715723e-11
+
+  defp round_to(val, precision), do: Float.round(val, precision)
 end
 
 defmodule Physics.Rocketry do
