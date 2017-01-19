a9b55636b81d1366d9138b8fd43002e740cd6fe7
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-19 15:05:18 -0800

    Add Ev calculations for mars and the moon.

--- test/physics_test.exs
+++ test/physics_test.exs
@@ -6,6 +6,17 @@ defmodule PhysicsTest do
     ev = Physics.Rocketry.escape_velocity(:earth)
     assert ev == 11.2
   end
+
+  test "escape velocity of mars is correct" do
+    ev = Physics.Rocketry.escape_velocity(:mars)
+    assert ev == 5.0
+  end
+
+  test "escape velocity of moon is correct" do
+    ev = Physics.Rocketry.escape_velocity(:moon)
+    assert ev == 2.4
+  end
+
   test "escape velocity of planet X is correct" do
     planet_x = %{mass: 4.0e22, radius: 6.21e6}
     ev = planet_x |> Physics.Rocketry.escape_velocity
