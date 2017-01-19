1e4a74cbaa8d82e84de7dbd098f24ff3eb251b8b
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-19 13:07:34 -0800

    Fix build warnings and update tests.

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -11,19 +11,19 @@ defmodule Converter do
     val * 1000
   end
 
-  def to_light_seconds({:miles, miles} = val) do
+  def to_light_seconds({:miles, miles} = _val) do
     (miles * 5.36819e-6) |> round_up
   end
 
-  def to_light_seconds({:meters, meters} = val)  do
+  def to_light_seconds({:meters, meters} = _val)  do
     (meters * 3.335638620368e-9) |> round_up
   end
 
-  def to_light_seconds({:feet, feet} = val)  do
+  def to_light_seconds({:feet, feet} = _val)  do
     (feet * 1.016702651488166404e-9) |> round_up
   end
 
-  def to_light_seconds({:inches, inches} = val)  do
+  def to_light_seconds({:inches, inches} = _val)  do
     (inches * 8.472522095734715723e-11) |> round_up
   end
 
--- test/physics_test.exs
+++ test/physics_test.exs
@@ -3,13 +3,12 @@ defmodule PhysicsTest do
   doctest Physics
 
   test "escape velocity of earth is correct" do
-    #notice how you can pipe directly to IO.inspect? And still get the value returned...
-    ev = Physics.Rocketry.escape_velocity(:earth) |> IO.inspect
-    assert ev == 0 #what should this value be?
+    ev = Physics.Rocketry.escape_velocity(:earth)
+    assert ev == 11.2
   end
   test "escape velocity of planet X is correct" do
     planet_x = %{mass: 4.0e22, radius: 6.21e6}
-    ev =  planet_x |> Physics.Rocketry.escape_velocity |> IO.inspect
-    #use assert here
+    ev = planet_x |> Physics.Rocketry.escape_velocity
+    assert ev == 1.0
   end
 end
